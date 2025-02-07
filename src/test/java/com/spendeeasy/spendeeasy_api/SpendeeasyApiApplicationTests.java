5833d8e48ea81cfb284c7a9c0e5d8f659bede024
kajal <kajaltiwary8484@gmail.com>
2025-02-08 00:19:21 +0530

    initial commit

---
+++ src/test/java/com/spendeeasy/spendeeasy_api/SpendeeasyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spendeeasy.spendeeasy_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendeeasyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
